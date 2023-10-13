f3c55452838021b748b41f8243cbe18b32e5c7a7
EduardMLT <123200877+EduardMLT@users.noreply.github.com>
2023-10-13 12:28:17 +0000

    Deploying to gh-pages from @ EduardMLT/goit-react-hw-05-movies@abf6bd0e59629ef3779f830eed4aba049154b135 🚀

---
+++ static/js/63.71478a51.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[63],{9063:function(e,t,n){n.r(t);var i=n(184);t.default=function(){return(0,i.jsxs)("main",{style:{textAlign:"center"},children:[(0,i.jsx)("b",{style:{fontSize:64},children:"404"}),(0,i.jsx)("p",{children:"Sorry, we couldn't find that movie :("})]})}}}]);
+//# sourceMappingURL=63.71478a51.chunk.js.map
